e4152463d90787d33f372f3fb981b5db41fbb5d8
Rahul <rahul.dewal@ahom.tech>
2023-05-20 11:02:37 +0530

    Create multiple child  element

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 const runReact = React.createElement(
     "div",
     { id: "parent" },
-    React.createElement("h1", {class:"red"}, "Created as Child")
+    [React.createElement("h1", {class:"red"}, "Created as Child"),
+    React.createElement("h1", {class:"red"}, "Created as Child"),
+    React.createElement("h1", {class:"red"}, "Created as Child"),
+    React.createElement("h1", {class:"red"}, "Created as Child"),
+    React.createElement("h1", {class:"red"}, "Created as Child"),
+    React.createElement("h1", {class:"red"}, "Created as Child"),
+]
   );
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(runReact);
