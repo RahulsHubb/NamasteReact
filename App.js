6757d6ba989de1a40eedabfac41ec39ae39aed93
Rahul <rahul.dewal@ahom.tech>
2023-05-20 10:31:16 +0530

    Create element by React JS

---
+++ App.js
@@ -0,0 +1,7 @@
+const runReact = React.createElement(
+    "h1",
+    { id: "Newh1" },
+    "Hello From React"
+  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(runReact);
